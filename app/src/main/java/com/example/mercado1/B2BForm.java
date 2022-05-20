21e63507ce6c1b0b80a62632a4b100508377c91b
Omar AL Agha <oea961@outlook.com>
2022-05-20 11:29:42 +0300

    V2

--- app/src/main/java/com/example/mercado1/B2BForm.java
+++ app/src/main/java/com/example/mercado1/B2BForm.java
@@ -7,32 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class B2B extends AppCompatActivity {
-
-    Button b1;
-
+public class B2BForm extends AppCompatActivity {
+Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_b2_b);
-        b1=(Button)findViewById(R.id.btnToForm);
+        setContentView(R.layout.activity_b2_bform);
+        b1=(Button) findViewById(R.id.btnSend);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i =new Intent(getBaseContext(),B2BForm.class);
+                Intent i =new Intent(getBaseContext(),A2.class);
+                i.putExtra("Code",123);
                 startActivity(i);
             }
         });
 
-    }
-    public void basket(View v)
-    {
-        Intent i1 = new Intent(this, A4.class);
-        startActivity(i1);
-    }
-    public void aboutUs(View v)
-    {
-        Intent i1 = new Intent(this, A8.class);
-        startActivity(i1);
     }
 }
