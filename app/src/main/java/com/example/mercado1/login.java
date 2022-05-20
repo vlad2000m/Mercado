5b3b40479fb37bd2be3f6320a3ff104be5f60f7d
vlad <vladimir8_makhoul@hotmail.com>
2022-05-20 16:06:54 +0300

    First Version

---
+++ app/src/main/java/com/example/mercado1/login.java
@@ -0,0 +1,21 @@
+package com.example.mercado1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+        Intent i1 = new Intent(this, A2.class);
+        startActivity(i1);
+    }
+}
